34c629bd756e54b4f0a09f4d96af7a992bd447aa
naeemahmedmb <naeemahmedmb@gmail.com>
2022-12-04 22:54:56 +0600

    pritter done

--- index.js
+++ index.js
@@ -1,6 +1,62 @@
-{/* <script type="module">
-  import { Toast } from 'bootstrap.esm.min.js'
+// {/* <script type="module">
+//   import { Toast } from 'bootstrap.esm.min.js'
 
-  Array.from(document.querySelectorAll('.toast'))
-    .forEach(toastNode => new Toast(toastNode))
-</script> */}
+//   Array.from(document.querySelectorAll('.toast'))
+//     .forEach(toastNode => new Toast(toastNode))
+// </script> */}
+// {/* <nav class="navbar navbar-expand-lg bg-info">
+// <div class="container-md">
+//   <a class="navbar-brand" href="http://shnyc.gov.bd"
+//     >শেখ হাসিনা জাতীয় যুব উন্নয়ন ইনস্টিটিউট, <br />যুব ও ক্রীড়া
+//     মন্ত্রণালয়, সাভার, ঢাকা।</a
+//   >
+// </div>
+// </nav> */}
+
+// <div class="carousel-control-next">
+//           <div
+//             id="carouselExampleControlsNoTouching"
+//             class="carousel slide"
+//             data-bs-touch="false"
+//           >
+//             <div class="carousel-inner">
+//               <div class="carousel-item active">
+//                 <img src="/jahid.jpg" class="d-block w-100" alt="..." />
+//               </div>
+//               <div class="carousel-item">
+//                 <img src="/mejbah.jpg" class="d-block w-100" alt="..." />
+//               </div>
+//               <div class="carousel-item">
+//                 <img
+//                   src="/SHNIYD-Cover-Pic..jpg"
+//                   class="d-block w-100"
+//                   alt="..."
+//                 />
+//               </div>
+//             </div>
+//             <button
+//               class="carousel-control-prev"
+//               type="button"
+//               data-bs-target="#carouselExampleControlsNoTouching"
+//               data-bs-slide="prev"
+//             >
+//               <span
+//                 class="carousel-control-prev-icon"
+//                 aria-hidden="true"
+//               ></span>
+//               <span class="visually-hidden">Previous</span>
+//             </button>
+//             <button
+//               class="carousel-control-next"
+//               type="button"
+//               data-bs-target="#carouselExampleControlsNoTouching"
+//               data-bs-slide="next"
+//             >
+//               <span
+//                 class="carousel-control-next-icon"
+//                 aria-hidden="true"
+//               ></span>
+//               <span class="visually-hidden">Next</span>
+//             </button>
+//           </div>
+//         </div>
